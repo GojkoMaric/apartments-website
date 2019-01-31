ed8bdca9897c430466349a9658c4d418774e11ea
GojkoMaric <gojko.maric@yahoo.com>
2019-01-31 22:55:31 +0100

    Added basic footer and header components

---
+++ src/components/Footer.vue
@@ -0,0 +1,97 @@
+<template>
+    <span>
+        <div class="footer">
+            <div>
+                <ul>
+                    <li><img src="../assets/logo.png"></li>
+                    <li>BE0677.970.513</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <h2>copandi</h2>
+                    <li>Blog</li>
+                    <li>Team</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <h2>vastgoed</h2>
+                    <li>Te koop</li>
+                    <li>Te huur</li>
+                    <li>Nieuwbouw</li>
+                    <li>Ik ben op zoek</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <h2>algemeen</h2>
+                    <li>Algemene voorwaarden</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <h2>contact</h2>
+                    <li>Leopolstraat 37, 2800 Mechelen</li>
+                    <li>info@copandi.be</li>
+                    <li>015 79 46 44</li>
+                </ul>
+            </div>
+            <div>
+                <h2>social</h2>
+            </div>
+        </div>
+        <div class="footer-copy">
+            <p>2018 copyright - alle rechten voorbehouden Copandi</p>
+        </div>
+    </span>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style lang="scss">
+$softDark: #242B33;
+$dark: #1B1E25;
+$greenText: #84C255;
+$greyText: #79808A;
+.footer {
+    background-color: $softDark;
+    min-height: 100px;
+    display: flex;
+    justify-content: space-evenly;
+    div {
+        flex-grow: 1;
+    }
+    img {
+        width: 50px;
+        height: 50px;
+    }
+    h2 {
+        text-transform: uppercase;
+        font-size: small;
+        color: $greenText;
+    }
+    li {
+        color: $greyText;
+        font-size: small
+    }
+}
+
+.footer-copy {
+    background-color: $dark;
+    justify-content: center;
+    display: flex;
+    height: 30px;
+    align-items: center;
+    p{
+        color: $greyText;
+        font-size: smaller;
+    }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class='back'>
+        <div class='header'>
+            <ul>
+                <li><img src="../assets/logo.png"></li>
+                <li>Te koop</li>
+                <li>Te huur</li>
+                <li>Neiuwbouw</li>
+                <li>Ik ben op zoek</li>
+                <li>Team</li>
+                <li>Prijzen</li>
+                <li>Blog</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style lang="scss">
+$softDark: #242B33;
+$dark: #1B1E25;
+$white: #fff;
+$headerHeight: 50px;
+.header {
+    background-color: $softDark;
+    height: $headerHeight;
+    overflow: hidden;
+    img {
+        width: 50px;
+        height: 50px;
+    }
+    ul{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        height: 100%;
+        width: 60%;
+        margin: 0 auto;
+        align-items: center;
+        li {
+            color: $white;
+            list-style-type: none;
+            font-size: small;
+            padding: 10% 0;
+            flex-grow: 1;
+            &:hover {
+                background-color: lightgray;
+                color: $softDark;
+                cursor: pointer;
+            }
+        }
+        li:first-child {
+            flex-grow: 2;
+        }
+    }
+}
+
+.back {
+    height: $headerHeight*3;
+    background-color: $dark;
+}
+
+</style>
