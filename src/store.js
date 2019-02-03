c5f226cf02ecf7547f19348ccad591a50cbeb594
GojkoMaric <gojko.maric@yahoo.com>
2019-02-03 14:08:13 +0100

    Added data to the project

--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import data from './assets/data/data.json'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    apartments: data
   },
   mutations: {
 
